c4b6660e62029fb3cf445b3f28ab9a4850538fb3
junior_escjr <junior_escjr@yahoo.com.br>
2025-03-02 19:42:42 -0300

    fix: corrigido o componente cards do dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,7 +7,12 @@ import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '@/app/lib/data
 const Page =  async () => {
     const revenue = await fetchRevenue();
     const latestInvoices = await fetchLatestInvoices();
-    const allCardData = await fetchCardData();
+    const {
+        numberOfInvoices,
+        numberOfCustomers,
+        totalPaidInvoices,
+        totalPendingInvoices,
+      } = await fetchCardData();
 
     return (
         <main>
@@ -15,10 +20,10 @@ const Page =  async () => {
                 Dashboard
             </h1>
             <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-                <Card title="Collected" value={allCardData.totalPaidInvoices} type="collected" />
-                <Card title="Pending" value={allCardData.totalPendingInvoices} type="pending" />
-                <Card title="Total Invoices" value={allCardData.numberOfInvoices} type="invoices" />
-                <Card title="Total Customers" value={allCardData.numberOfCustomers} type="customers" />
+                <Card title="Collected" value={totalPaidInvoices} type="collected" />
+                <Card title="Pending" value={totalPendingInvoices} type="pending" />
+                <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+                <Card title="Total Customers" value={numberOfCustomers} type="customers" />
             </div>
             <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
                 <RevenueChart revenue={revenue}  />
